5724ac57b738635b7d49fdc0447b44cf0385e9a9
Nihal Tiwari <nihutiwari978@gmail.com>
2023-12-17 20:17:40 +0530

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ function AdmissionForm() {
       if (response.data.paymentResult.success) {
         setPaymentStatus("Payment successful");
       } else {
+        console.log(response.data);
         setPaymentStatus("Payment failed");
       }
     } catch (error) {
@@ -34,13 +35,13 @@ function AdmissionForm() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-green-400 to-blue-500">
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
       <form
         onSubmit={handleSubmit}
-        className="w-full max-w-md px-8 py-6 space-y-6 bg-white rounded shadow-xl"
+        className="w-full max-w-md px-8 py-6 space-y-6 bg-white rounded shadow-2xl"
       >
-        <h2 className="text-2xl font-bold text-center text-gray-700">
-          Admission Form
+        <h2 className="text-3xl font-extrabold text-center text-gray-800">
+          Admission Form for Yoga Classes
         </h2>
         {error && <p className="text-red-500">{error}</p>}
         {paymentStatus && <p className="text-green-500">{paymentStatus}</p>}
@@ -48,7 +49,7 @@ function AdmissionForm() {
           <label className="block w-full">
             <span className="text-gray-700">Name:</span>
             <input
-              className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+              className="w-full px-4 py-2 mt-2 border-2 border-purple-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600"
               type="text"
               value={name}
               onChange={(e) => setName(e.target.value)}
@@ -60,7 +61,7 @@ function AdmissionForm() {
           <label className="block w-full">
             <span className="text-gray-700">Age:</span>
             <input
-              className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+              className="w-full px-4 py-2 mt-2 border-2 border-purple-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600"
               type="number"
               value={age}
               min="18"
@@ -74,7 +75,7 @@ function AdmissionForm() {
           <label className="block w-full">
             <span className="text-gray-700">Batch:</span>
             <select
-              className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+              className="w-full px-4 py-2 mt-2 border-2 border-purple-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-600"
               value={batch}
               onChange={(e) => setBatch(e.target.value)}
               required
@@ -90,7 +91,7 @@ function AdmissionForm() {
         <div className="flex flex-wrap">
           <button
             type="submit"
-            className="w-full px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400"
+            className="w-full px-4 py-2 text-white bg-purple-500 rounded-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600"
           >
             Submit
           </button>
